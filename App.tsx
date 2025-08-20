6d9338666322b15b7b7b5f3f34fa9c5d81ffd5a0
dung11122005 <dungakaishi900@gmail.com>
2025-08-20 10:46:17 +0700

    1

--- App.tsx
+++ App.tsx
@@ -1,11 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hoang Tan Dung, react native!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text style={styles.header}>Hoang tan dung</Text>
+        <Text style={styles.parent}>
+          1
+          <Text style={styles.child}>2</Text>
+        </Text>
+      </View>
+      <Text style={styles.hello1}>
+        Hoang Tan Dung 1</Text>
+      <Text>Hoang Tan Dung 2</Text>
     </View>
   );
 }
@@ -17,4 +24,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  hello1: {
+    color: "red", fontSize: 40,
+    borderColor: "green",
+    borderWidth: 1,
+    padding: 10
+  },
+  header: {
+    fontSize: 30,
+    fontWeight: "600"
+  },
+  parent: {
+    fontSize: 30,
+    color: "green"
+  },
+  child: {
+    fontSize: 30,
+    color: "pink"
+  }
 });
